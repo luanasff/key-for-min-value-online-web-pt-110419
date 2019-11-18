5a89e55922e44a81d5d1e7cedc57841f0f7f30f9
Luana Faria Lane <luanasfff@gmail.com>
2019-11-18 22:33:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,19 @@
 
 
  
-def key_for_min_value(name_hash)
- 
+
+ def key_for_min_value(name_hash)
+  lowest_value = nil
+  lowest_key = nil
+  name_hash.each do |key, value|
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
+    end 	   
+
+  end
+  lowest_key
+end
  
 end 
 
